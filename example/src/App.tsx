fb71453f8aeacf76be15c6d8a938a1aafcd524a8
Tom Heaton <thomas.m.heaton@gmail.com>
2024-06-14 21:36:42 +0200

    cleanup

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -6,11 +6,30 @@ export default function App() {
     <main>
       <h1>MorphIcons Example App</h1>
       <br />
-      <div style={{ display: "flex", gap: 16, alignItems: "center", justifyContent: "center" }}>
-        <MorphIcon name="sun" color="orange" size={48} variant="circle" fill designVariant="filled" />
+      <div
+        style={{
+          display: "flex",
+          gap: 16,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <MorphIcon
+          name="sun"
+          color="orange"
+          size={48}
+          variant="circle"
+          fill
+          designVariant="filled"
+        />
         <MorphIcon name="sun" color="blue" size={48} variant="square" />
         <MorphIcon name="sun" color="red" size={48} variant="default" />
-        <MorphIcon name="moon" color="yellow" size={48} designVariant="filled" />
+        <MorphIcon
+          name="moon"
+          color="yellow"
+          size={48}
+          designVariant="filled"
+        />
       </div>
     </main>
   );
--- morphicons/src/index.ts
+++ morphicons/src/index.ts
@@ -1,13 +1,1 @@
-export { default as Icon } from "./Icon";
-
-export { default as CircleIcon } from "./CircleIcon";
-export { default as SquareIcon } from "./SquareIcon";
-
-export { default as TestIcon } from "./TestIcon";
-
 export { default as MorphIcon } from "./MorphIcon";
-
-export type TestType = {
-  name: string;
-  size: number;
-};
